565331f1e737e5b719cdd6b789852e5cdb265c59
nkemjiks <mbonunkemjika@ymail.com>
2018-12-11 16:20:16 +0100

    - Making queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,39 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    description "root query"
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :authors, [AuthorType], null: true do
+      description "gets all authors"
+    end
+
+    field :author, AuthorType, null: true do
+      description "Find an author by id"
+      argument :id, ID, required: true
+    end
+
+    field :books, [BookType], null: true do
+      description "Get all Books"
+    end
+  
+    field :book, BookType, null: true do
+      description "Find a book by id"
+      argument :id, ID, required: true
+    end
+
+    def authors
+      Author.all
+    end
+
+    def author(id:)
+      Author.find(id)
+    end
+
+    def books
+      Book.all
+    end
+
+    def book(id:)
+      Book.find(id)
     end
   end
 end
